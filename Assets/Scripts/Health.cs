2a9ee84d66aab65ce723d394219fe98bd589cfe4
Leo V <41674331+LeoVirolainen@users.noreply.github.com>
2022-02-10 12:41:17 +0200

    Rudimentary sound effects

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,20 +5,26 @@ using System;
 
 public class Health : MonoBehaviour, IDamageable
 {
-
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip hurtSound;    
     [SerializeField] private int maxHealth = 100;
     public int currentHealth;
     public event EventHandler OnDeath;
 
     void Start()
     {
-        currentHealth = maxHealth;        
+        currentHealth = maxHealth;
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void TakeDamage(int damage)
     {
+        audioSource.PlayOneShot(hurtSound);
+
         currentHealth -= damage;
         if (currentHealth <= 0) { currentHealth = 0; OnDeath?.Invoke(this, EventArgs.Empty); }
+
+
         
     }    
 }
