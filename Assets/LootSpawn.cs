86ca9ad15fb8b7dc2e3ce64ecbcfcaa32ab4c00e
Leo V <41674331+LeoVirolainen@users.noreply.github.com>
2022-02-14 13:01:41 +0200

    Loot drop update

--- Assets/LootSpawn.cs
+++ Assets/LootSpawn.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LootSpawn : MonoBehaviour {
-    [SerializeField] private GameObject experiencePoint;
-    [SerializeField] private GameObject perkPoint;
-    [SerializeField] private GameObject healthPack;
+    private float dropRandomizer;
+    private GameObject healthPack;
 
-    [SerializeField] private int amountToSpawn;
-    // Start is called before the first frame update
+    [SerializeField] private GameObject goldPiece;
+    [SerializeField] private bool willDropHP = false;
+
+    [Range(0.0f, 1.0f)] //inspector slider for the float below
+    [SerializeField] private float healthPackDropChance = 0.5f;
+    [Range(0, 20)] //inspector slider for the float below
+    [SerializeField] private int goldAmtToDrop = 10;
+
     void Start() {
-        for (int i = 0; i < amountToSpawn; i++) {
-            var position = new Vector3(Random.Range(-0.1f, 0.1f), 0, Random.Range(-0.1f, 0.1f));
-            Instantiate(perkPoint, position, Quaternion.identity);
+        for (int i = 0; i < goldAmtToDrop; i++) {
+            Instantiate(goldPiece, transform.position, Random.rotation, gameObject.transform);
+        }
+
+        healthPack = GetComponentInChildren<HealthPack>().gameObject;
+
+        dropRandomizer = Random.Range(0, 2);
+        if (dropRandomizer <= healthPackDropChance) {
+            willDropHP = true;
+
+        } else {
+            willDropHP = false;
+        }
+        if (willDropHP == false) {
+            Destroy(healthPack);
+        }
+    }
+    private void Update() {
+        if (healthPack = null) {
+            Destroy(gameObject);
         }
     }
 }
+
