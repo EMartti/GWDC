fd8b2ddc9810425b89211f6aefe7f90259d5fa2d
EMartti <55134546+EMartti@users.noreply.github.com>
2022-02-28 13:06:54 +0200

    Added character class script

---
+++ Assets/Scripts/CharacterClass.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterClass : MonoBehaviour
+{
+    public Class characterClass;
+}
+
+public enum Class { Melee, Range, Mage }
